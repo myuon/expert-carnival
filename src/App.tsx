af7c2c9cd2f88f297009e923fb445cc5e3168093
myuon <ioi.joi.koi.loi@gmail.com>
2022-10-09 14:38:42 +0900

    feat: rotation

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import "./App.css";
 import { Image } from "image-js";
-import { useEffect, useRef, useState } from "react";
+import { useCallback, useEffect, useRef, useState } from "react";
 import { css } from "@emotion/react";
 import { Histogram } from "./Histogram";
 import { convertImage } from "./helper/image";
@@ -19,22 +19,40 @@ const useFetchImage = (url: string) => {
 
 export const App = () => {
   const canvasRef = useRef<HTMLCanvasElement | null>(null);
-  const [image, setImage] = useState<Image>();
   const { image: originalImage, histogram } = useFetchImage(
     "https://picsum.photos/seed/picsum/800/550"
   );
 
-  useEffect(() => {
-    if (originalImage) {
-      setImage(originalImage);
+  const [toneCurve, setToneCurve] = useState<number[]>([]);
+  const [rotation, setRotation] = useState(0);
+
+  const update = useCallback(() => {
+    if (!originalImage) {
+      return;
     }
-  }, [originalImage]);
+
+    const newImage = convertImage(
+      originalImage,
+      (v) => toneCurve[Math.floor((v * 100) / 256)] * 256
+    ).rotate(rotation);
+
+    canvasRef.current
+      ?.getContext("2d")
+      ?.clearRect(0, 0, canvasRef.current.width, canvasRef.current.height);
+    canvasRef.current
+      ?.getContext("2d")
+      ?.drawImage(
+        newImage.getCanvas(),
+        (800 - newImage.width) / 2,
+        (550 - newImage.height) / 2
+      );
+  }, [originalImage, rotation, toneCurve]);
 
   useEffect(() => {
-    if (image) {
-      canvasRef.current?.getContext("2d")?.drawImage(image.getCanvas(), 0, 0);
+    if (originalImage) {
+      update();
     }
-  }, [image]);
+  }, [originalImage, update]);
 
   return (
     <div className="App">
@@ -53,20 +71,62 @@ export const App = () => {
             background-color: black;
           `}
         />
-        <Histogram
-          values={histogram}
-          onChangeToneCurve={(points) => {
-            if (originalImage) {
-              const newImage = image
-                ? convertImage(
-                    originalImage,
-                    (v) => points[Math.floor((v * 100) / 256)] * 256
-                  )
-                : undefined;
-              setImage(newImage);
-            }
-          }}
-        />
+        <div
+          css={css`
+            display: grid;
+            gap: 16px;
+            align-self: flex-start;
+          `}
+        >
+          <section
+            css={css`
+              display: grid;
+              gap: 8px;
+              align-self: flex-start;
+            `}
+          >
+            <span
+              css={css`
+                font-weight: 600;
+              `}
+            >
+              histogram
+            </span>
+            <Histogram
+              values={histogram}
+              onChangeToneCurve={(points) => {
+                setToneCurve(points);
+                update();
+              }}
+            />
+          </section>
+          <section
+            css={css`
+              display: grid;
+              gap: 8px;
+              align-self: flex-start;
+            `}
+          >
+            <span
+              css={css`
+                font-weight: 600;
+              `}
+            >
+              rotation
+            </span>
+            <input
+              type="range"
+              min={-180}
+              max={180}
+              defaultValue={0}
+              onChange={(event) => {
+                const rotation = Number(event.target.value);
+                setRotation(rotation);
+                update();
+              }}
+            />
+          </section>
+        </div>
       </div>
     </div>
   );
